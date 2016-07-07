a249b7d144833175c3a4a43a4c09e1c350034229
Gianni Moschini <gianni.moschini@hailocab.com>
2016-07-07 11:42:03 +0100

    Add option to fail and retry

--- examples/main.go
+++ examples/main.go
@@ -17,6 +17,9 @@ type Job struct {
 	Wg   *sync.WaitGroup
 	Rnd *rand.Rand
 	JobQueue chan worker.Job
+	RandomFail bool
+	Retries int
+	Attempts int
 }
 
 func (j Job) String() string {
@@ -25,7 +28,7 @@ func (j Job) String() string {
 
 func (j Job) Process() error {
 	fmt.Printf("Processing job %s\n", j.String())
-	if j.Rnd.Intn(2) == 0 {
+	if !j.RandomFail || j.Rnd.Intn(2) == 0 {
 		return nil
 	}
 	return fmt.Errorf("Some kind of error")
@@ -34,10 +37,17 @@ func (j Job) Process() error {
 func (j Job) Result(err error) {
 	if err != nil {
 		fmt.Printf("Processing job %s failed: %s\n", j.String(), err)
-		j.JobQueue <- j
-		return
+		if j.Attempts < j.Retries {
+			j.Attempts++
+			fmt.Printf("Retrying job %s [Retries:%d]\n", j.String(), j.Attempts)
+			j.JobQueue <- j
+			return
+		} else {
+			fmt.Printf("Giving up job %s [Retries:%d]\n", j.String(), j.Attempts)
+		}
+	} else {
+		fmt.Printf("Processed job %s successfully\n", j.String())
 	}
-	fmt.Printf("Processed job %s successfully\n", j.String())
 	j.Wg.Done()
 }
 
@@ -47,6 +57,8 @@ func main() {
 	var jobs = flag.Int("j", 100, "Number of jobs")
 	var workers = flag.Int("w", runtime.NumCPU(), "Number of workers")
 	var queues = flag.Int("q", 2, "Number of queues")
+	var fail = flag.Bool("f", false, "Fail randomly")
+	var retries = flag.Int("r", 1, "Number of retries for failed jobs")
 	flag.Parse()
 
 	dispatcher := worker.NewDispatcher(*workers)
@@ -56,7 +68,7 @@ func main() {
 	for i := 0; i < *jobs; i++ {
 		wg.Add(1)
 		fmt.Printf("Adding job %d to the queue\n", i)
-		JobQueue <- Job{Name: fmt.Sprintf("%d", i), Wg: wg, Rnd: r, JobQueue: JobQueue}
+		JobQueue <- Job{Name: fmt.Sprintf("%d", i), Wg: wg, Rnd: r, JobQueue: JobQueue, RandomFail: *fail, Retries: *retries}
 	}
 
 	wg.Wait()
